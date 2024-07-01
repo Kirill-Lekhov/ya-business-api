f34ffefde752697451a0c933038efe38208e7974
Kirill_Lekhov <Kirill.Lekhov@gmail.com>
2024-07-01 11:51:19 +0300

    Fix getting CSRF token in the AsyncServiceAPI

--- ya_business_api/service/async_api.py
+++ ya_business_api/service/async_api.py
@@ -9,6 +9,7 @@ from logging import getLogger; log = getLogger(__name__)
 class AsyncServiceAPI(AsyncAPIMixin, BaseServiceAPI):
 	async def get_csrf_token(self) -> Optional[str]:
 		url = self.router.csrf_token()
+		self.set_i_cookie()
 		time_start = monotonic()
 
 		async with self.session.post(url, allow_redirects=False) as response:
