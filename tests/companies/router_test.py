e5820530d1bbd2916d185a97075c42510f9a65e8
Kirill_Lekhov <Kirill.Lekhov@gmail.com>
2025-10-02 16:06:35 +0300

    Fix test name

--- tests/companies/router_test.py
+++ tests/companies/router_test.py
@@ -6,6 +6,6 @@ class TestCompaniesRouter:
 		router = CompaniesRouter()
 		assert router.companies() == "https://yandex.ru/sprav/api/companies"
 
-	def test_chain_list(self):
+	def test_chain_branches(self):
 		router = CompaniesRouter()
 		assert router.chain_branches(1) == "https://yandex.ru/sprav/api/chain/1/branches/"
